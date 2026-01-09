bd33688a3f995b146bd1f21e624199b1d52558b8
itsuttamsah <itsuttamsah@gmail.com>
2026-01-09 23:45:00 +0545

    Layout Management

---
+++ Chapter3/GridLayoutDemo.java
@@ -0,0 +1,33 @@
+import java.awt.*;
+import javax.swing.*;
+
+public class GridLayoutDemo extends JFrame
+{
+
+    public GridLayoutDemo()
+    {    
+        setTitle("GridLayout");
+        setSize(400, 400);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new GridLayout(3, 2));
+
+        JLabel userLabel = new JLabel("Username");
+        JLabel passwordLable = new JLabel("Password");
+        JTextField usertext = new JTextField();
+        JTextField userPassword = new JTextField();
+        JButton cancelBtn = new JButton("Cancel");
+        JButton okBtn = new JButton ("OK");
+
+        add(userLabel);
+        add(usertext);
+        add(passwordLable);
+        add(userPassword);
+        add(okBtn);
+        add(cancelBtn);
+
+        setVisible(true);
+    }
+    public static void main(String[] args) {
+        new GridLayoutDemo();
+    }
+}
